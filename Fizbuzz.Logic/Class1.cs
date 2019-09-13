836e65e54ce625a48e51f27973568a22f0ae5b3d
Sergio A <saguado78@gmail.com>
2019-09-13 12:35:59 +0200

    Green. Just enough to pass the test

--- Fizbuzz.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Fizbuzz.Logic
-{
-    public class Class1
-    {
-    }
-}
---
+++ Fizbuzz.Logic/FizzBuzzCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fizbuzz.Logic
+{
+    public class FizzBuzzCalculator
+    {
+        public string Calculate(int input)
+        {
+            return input.ToString();
+        }
+    }
+}
