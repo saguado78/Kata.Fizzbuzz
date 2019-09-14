de7596d7183a0abf7c1c456829c92fb990aa4cea
Sergio A <saguado78@gmail.com>
2019-09-14 18:29:09 +0200

    Red. New test case: divisible by 3

--- Fizbuzz.Tests/FizzBuzzCalculator_should.cs
+++ Fizbuzz.Tests/FizzBuzzCalculator_should.cs
@@ -13,5 +13,16 @@ namespace Fizbuzz.Tests
 
             Assert.AreEqual("1",res,"Unexpected resutl");
         }
+
+        [Test]
+        [TestCase(3)]
+        [TestCase(6)]
+        public void return_FIZZ_when_divisible_by_3(int input){
+            FizzBuzzCalculator calc=new FizzBuzzCalculator();
+
+            string res=calc.Calculate(input);
+
+            Assert.AreEqual("FIZZ",res,"Unexpected resutl");
+        }
     }
 }
